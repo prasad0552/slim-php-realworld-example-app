f5c28be592b48c646957ea5e84ce67727f2a4976
upendra.manve <upendramanve@gmail.com>
2018-02-06 15:38:43 +0530

    Routine commit: Promo

--- public/js/add_promotion.js
+++ public/js/add_promotion.js
@@ -23,24 +23,17 @@ $(document).ready(function(){
         }
     });
 
-    $("#promotion_data").click(function(event){
+    $("#promotion_data_btn").click(function(event){
 
 
-
-        /*$.post(
-            baseUrl+"/slim_mvc/public/admin/discount",
-
-            function(data) {
-                console.log(data);
-            }
-        );*/
 
         jQuery.ajax({
             type: "POST",
-            url:  baseUrl+"/slim_mvc/public/admin/discount",
+            url:  basuri+"/admin/discount",
             data: $("#promotion_data").serialize(), // serializes the form's elements.
             success: function(data) {
                 console.log(data); // show response from the php script. (use the developer toolbar console, firefox firebug or chrome inspector console)
+                location.href = basuri+'/admin/discounts';
             }
         });
 
