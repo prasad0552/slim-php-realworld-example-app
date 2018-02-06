b3d5bc60678b915f94e11ff3aa0cf59a54fd0025
upendra.manve <upendramanve@gmail.com>
2018-02-06 10:53:40 +0530

    Routine commit: Twig integration

--- src/dependencies.php
+++ src/dependencies.php
@@ -36,6 +36,17 @@ $container['logger'] = function ($c) {
     return $logger;
 };
 
+// Register Twig View helper
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('View', []);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new \Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
 // Jwt Middleware
 $container['jwt'] = function ($c) {
 
