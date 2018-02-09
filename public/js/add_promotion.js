af67dc0a7fc23165b631c96707ae05dd49b74253
hathiramakrishna <ramakrishna.hathi@jdplc.com>
2018-02-09 11:47:03 +0530

    added channel and exclusive column

--- public/js/add_promotion.js
+++ public/js/add_promotion.js
@@ -1,7 +1,76 @@
 $(document).ready(function(){
 
 
-    var baseUrl = document.location.origin;
+            $("#promotion_data").validate({
+                rules: {
+                    name: {
+                        required: true
+                    },
+                    type: {
+                        required: true
+                    },
+                    promo: {
+                        required: true
+                    },
+                    start_date: {
+                        required: true,
+                        date: true
+                    },
+                    end_date: {
+                        required: true,
+                        date: true
+                    },
+                    percent_off: {
+                        required: function(element) {
+                            if ($('#percentage_discount').val=='percentage_discount') {
+                                return false;
+                            } else {
+                                return true;
+                            }
+                        },
+                        number: true
+                    },
+                    fixed_off: {
+                        required: function(element) {
+                            if ($('#fixed_field').val=='fixed_amount') {
+                                return false;
+                            } else {
+                                return true;
+                            }
+                        },
+                        number: true
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "Name is required"
+                    },
+                    type: {
+                        required: "Type is required"
+                    },
+                    promo: {
+                        required: "Promo is required"
+                    },
+                    start_date: {
+                        required: "Start Date is required",
+
+                    },
+                    end_date: {
+                        required: "End Date is required",
+
+                    },
+                    percent_off: {
+                        required: "Percent is required",
+
+                    },
+                    fixed_off: {
+                        required: "Amount is required",
+
+                    }
+
+                },
+            });
+
 
     $( "#promotion_start_date,#promotion_end_date" ).datepicker({
         changeYear: true
@@ -25,21 +94,34 @@ $(document).ready(function(){
         }
     });
 
-    $("#promotion_data_btn").click(function(event){
 
 
 
-        jQuery.ajax({
-            type: "POST",
-            url:  basuri+"/admin/discount",
-            data: $("#promotion_data").serialize(), // serializes the form's elements.
-            success: function(data) {
-                console.log(data); // show response from the php script. (use the developer toolbar console, firefox firebug or chrome inspector console)
-                location.href = basuri+'/admin/discounts';
+
+        $("#promotion_data_btn").click(function (event) {
+
+
+            var val=$("#promotion_data").valid();
+
+                if( val!=false) {
+
+                jQuery.ajax({
+                    type: "POST",
+                    url: basuri + "/admin/discount",
+                    data: $("#promotion_data").serialize(), // serializes the form's elements.
+                    success: function (data) {
+                        console.log(data); // show response from the php script. (use the developer toolbar console, firefox firebug or chrome inspector console)
+                        location.href = basuri + '/admin/discounts';
+                    }
+                });
             }
+            else
+                {
+
+                    return false;
+                }
+
         });
 
-
-    });
 
 });
